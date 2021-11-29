def5bbe27ea25526bbb0044099b4fa4b821ee79f
paaaarv <pkumar@trinity.edu>
2021-11-29 17:34:04 -0600

    sorting algorithm

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 function findMinAndRemove(array){
+    let num=array[0];
+    let minIndex=0
+    if(array.length <=1){
+        return array[0]
+    }
+    else{
+        for(let i=0; i<array.length; i++){
+            if (array[i]<num){
+                num=array[i]
+                minIndex=i
+            }
 
+        }
+        array.splice(minIndex,1)
+        return num
+    }
 }
 
-function selectionSort(array){
-
+function selectionSort(array,newArray=[]){
+    if(array.length<=1){
+        newArray.push(findMinAndRemove(array))
+        return newArray;
+    }else{
+        newArray.push(findMinAndRemove(array))
+        return selectionSort(array,newArray)
+    }
 }
